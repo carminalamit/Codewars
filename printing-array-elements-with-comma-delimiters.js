44b03d6c99ed11ee02d32405e42963fb34de974b
minalamit <lamitcarmina@gmail.com>
2023-05-13 23:43:08 +0800

    added new codewars challenge

---
+++ printing-array-elements-with-comma-delimiters.js
@@ -0,0 +1,6 @@
+function printArray(array) {
+  return array.join(",");
+}
+
+// join() method is used to concatenate all the elements of an array into a string. 
+// By default, join() uses a comma as the separator between elements
