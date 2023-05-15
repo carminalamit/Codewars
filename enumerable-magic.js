6ebc4b08762bb3a45b34681d010092f79d0b2e5f
minalamit <lamitcarmina@gmail.com>
2023-05-15 13:02:11 +0800

    added new codewars challenge

---
+++ enumerable-magic.js
@@ -0,0 +1,20 @@
+// Instruction
+// Create a method all which takes two params:
+
+// a sequence
+// a function (function pointer in C)
+// and returns true if the function in the params returns true for every element in the sequence.
+// Otherwise, it should return false. If the sequence is empty, it should return true, since technically nothing failed the test.
+
+// Example
+// all((1, 2, 3, 4, 5), greater_than_9) -> false
+// all((1, 2, 3, 4, 5), less_than_9)    -> True
+
+// My Solution
+function all(arr, fun) {
+  return arr.every(fun);
+}
+
+// every() method returns true if the function returns true for all elements.
+
+// every() method returns false if the function returns false for one element.
