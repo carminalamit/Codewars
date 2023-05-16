11acf8513cbf2cc31686f70097febc5530fef00a
minalamit <lamitcarmina@gmail.com>
2023-05-16 23:14:34 +0800

    added new codewars challenge

---
+++ put-a-hash.js
@@ -0,0 +1,10 @@
+// Instructions
+// You are trying to put a hash in ruby or an object in javascript or java into an array, 
+// // but it always returns error, solve it and keep it as simple as possible!
+
+// items = []
+// items.push {a: "b", c: "d"}
+
+// My Solution
+const items = []
+items.push({a: "b", c: "d"})
