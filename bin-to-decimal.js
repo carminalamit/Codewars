5b17e2e21e433f5746cd10441952ebe4675e8301
minalamit <lamitcarmina@gmail.com>
2023-06-23 23:02:31 +0800

    added new codewars challeng

---
+++ bin-to-decimal.js
@@ -0,0 +1,10 @@
+// Instructions
+
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+// My Solution
+// convert binary to decimal using parseInt function
+
+function binToDec(bin) {
+  return parseInt(bin, 2);
+}
