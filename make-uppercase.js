de788e525d58b6d463889a9e10d09754270ed23a
minalamit <lamitcarmina@gmail.com>
2023-06-23 23:22:57 +0800

    added new codewars challeng

---
+++ make-uppercase.js
@@ -0,0 +1,11 @@
+// Instructions
+
+// Write a function which converts the input string to uppercase.
+
+// My Solution
+
+// to convert string to uppercase we can use toUpperCase() method
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
