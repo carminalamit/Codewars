03a74734000a04124fb60152b78eb1f277e67365
minalamit <lamitcarmina@gmail.com>
2023-06-24 16:33:44 +0800

    added new codewars challenge

---
+++ convert-bool-to-string.js
@@ -0,0 +1,13 @@
+// Instructions
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// My Solution
+
+// to convert boolean to string we can use toString method
+
+function booleanToString(b) {
+  return b.toString();
+}
