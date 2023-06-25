1241872d5269cec8c38216c074a87dca21c8e844
minalamit <lamitcarmina@gmail.com>
2023-06-25 22:14:23 +0800

    added new codewars challenge

---
+++ find-the-integral.js
@@ -0,0 +1,34 @@
+// Instructions
+
+// Create a function that finds the integral of the expression passed.
+
+// In order to find the integral all you need to do is add one to the exponent (the second argument), and divide the coefficient (the first argument) by that new number.
+
+// For example for 3x^2, the integral would be 1x^3: we added 1 to the exponent, and divided the coefficient by that new number).
+
+// Notes:
+
+// The output should be a string.
+// The coefficient and exponent is always a positive integer.
+// Examples
+//  3, 2  -->  "1x^3"
+// 12, 5  -->  "2x^6"
+// 20, 1  -->  "10x^2"
+// 40, 3  -->  "10x^4"
+// 90, 2  -->  "30x^3"
+
+// My Solution
+
+function integrate(coefficient, exponent) {
+  // Step 1: Compute the new exponent
+  const newExponent = exponent + 1;
+
+  // Step 2: Divide the coefficient by the new exponent
+  const newCoefficient = coefficient / newExponent;
+
+  // Step 3: Create the string representation of the integral
+  const integral = `${newCoefficient}x^${newExponent}`;
+
+  // Step 4: Return the resulting string
+  return integral;
+}
