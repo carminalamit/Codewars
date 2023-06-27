2efbaa0f9d9ca3b1345135ab1bac657e2b15e0e5
minalamit <lamitcarmina@gmail.com>
2023-06-27 23:29:52 +0800

    added new codewars challenge

---
+++ return-string.js
@@ -0,0 +1,14 @@
+// Instructions
+
+// Make a function that will return a greeting statement that uses an input; your program should
+// return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+// My Solution
+
+// we can use template literals
+
+function greet(name) {
+  return `Hello, ${name} how are you doing today?`;
+}
