0c9557cea1e1bb63a11f0502458f61718e6b3a0c
minalamit <lamitcarmina@gmail.com>
2023-06-28 12:47:12 +0800

    added new codewars challenge

--- uppercase-string.js
+++ uppercase-string.js
@@ -14,9 +14,6 @@
 // In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
 
 // My Solution
-String.prototype.isUpperCase = function () {
-  return this === this.toUpperCase() || !/[a-z]/.test(this);
-};
 
 // Within the method, this refers to the string object on which the method is called.
 // The toUpperCase() method converts the string to uppercase. If the resulting string is the same as the original string, it means all characters were already uppercase, so we return true.
@@ -24,3 +21,12 @@ String.prototype.isUpperCase = function () {
 // If the string is either all uppercase or does not contain any lowercase letters, we return true.
 // If none of the above conditions are met, the string is not in ALL CAPS, so we return false.
 // Here are some examples using the method:
+String.prototype.isUpperCase = function () {
+  return this === this.toUpperCase() || !/[a-z]/.test(this);
+};
+
+// other solution
+
+String.prototype.isUpperCase = function () {
+  return this == this.toUpperCase();
+};
