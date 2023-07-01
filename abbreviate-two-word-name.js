49b051ab5ef42b4cbc8d8cd972c8a531f8964ef9
minalamit <lamitcarmina@gmail.com>
2023-07-01 22:30:01 +0800

    added new codewars challenge

---
+++ abbreviate-two-word-name.js
@@ -0,0 +1,22 @@
+// Instructions
+
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+// My Solution
+
+function abbrevName(name) {
+  // We use the split(" ") method to split the name string into an array of two words.
+  const names = name.split(" ");
+  // We access the first character of the first word using names[0][0], convert it to uppercase using toUpperCase(),
+  // and concatenate it with a dot (.) and the first character of the second word (names[1][0]), which is also converted to uppercase.
+  const initials = names[0][0].toUpperCase() + "." + names[1][0].toUpperCase();
+  return initials;
+}
