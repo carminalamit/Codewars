ee7b8336ce04d98c6f0c0ebadadd23ba1e94ccef
minalamit <lamitcarmina@gmail.com>
2023-07-06 21:33:55 +0800

    added new codewars challenge

---
+++ volume-of-a-cuboid.js
@@ -0,0 +1,16 @@
+// Instructions
+
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid.
+// Write a function to help Bob with this calculation.
+
+// My Solution
+
+// Calculate the volume using the formula volume = length * width * height.
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    // your code here
+    const volume = length * width * height;
+    return volume;
+  }
+}
