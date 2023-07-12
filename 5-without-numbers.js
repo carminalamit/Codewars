d3eeee72f593c7e646a3df9402dc92e2016fc7d2
minalamit <lamitcarmina@gmail.com>
2023-07-12 23:08:28 +0800

    added new codewars challenge

---
+++ 5-without-numbers.js
@@ -0,0 +1,14 @@
+// Instructions
+
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+// My Solution
+
+function unusualFive() {
+  let str = "hello";
+  return str.length;
+}
