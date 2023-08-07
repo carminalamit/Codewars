f48ee06e8c4f093361dff056e905eae536a3711c
minalamit <lamitcarmina@gmail.com>
2023-08-07 18:34:56 +0800

    added new codewars challenge

--- total-pressure-calculation.js
+++ total-pressure-calculation.js
@@ -221,3 +221,8 @@ solution = (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) => {
 
   return totalPressure;
 };
+
+// other solution
+solution= (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) => {
+  return (givenMass1/molarMass1 + givenMass2/molarMass2) * 0.082 * (temp+273.15) / volume;
+}
